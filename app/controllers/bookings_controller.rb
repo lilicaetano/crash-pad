bffd99d19fc7f3bba5c13e47c2b3a4d40d62c929
LilianaCaetano <lili.caetano1@gmail.com>
2019-08-03 16:23:43 +0100

    flats controller + bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,23 @@
 class BookingsController < ApplicationController
+
+def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to flat_path(@flat)
+    else
+      render 'new'
+    end
+  end
+
+private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :guests)
+  end
+end
+
 end
