ed1e6d83284c5ffb2f4c22719b59c5852f020782
Alexander Stones <amstones@hotmail.co.uk>
2019-08-06 20:10:04 +0100

    created base flat booking form, needs validation for acceptable dates

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,17 @@
 class BookingsController < ApplicationController
   def new
+    @flat = Flat.find(params[:flat_id])
     @booking = Booking.new
   end
 
   def create
     @booking = Booking.new(booking_params)
+    @booking.flat = Flat.find(params[:flat_id])
+    @booking.user = current_user
     if @booking.save
-      redirect_to flat_path(@flat)
+      redirect_to flat_path(@booking.flat)
     else
-      render 'new'
+      render :new
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,4 @@ Rails.application.routes.draw do
   resources :flats do
     resources :bookings
   end
-
-
 end
