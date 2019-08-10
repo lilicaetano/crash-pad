b466c76033fc6a609c97d9f0698a736d511964ef
Alexander Stones <amstones@hotmail.co.uk>
2019-08-10 14:08:49 +0100

    added user index route to allow viewing of their bookings, including a link to page in nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'users/bookings', to: 'users#bookings'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :flats do
-    resources :bookings
+    resources :bookings, only: [:new, :create]
   end
+
+
 end
