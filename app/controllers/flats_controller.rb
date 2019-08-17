2ce07d166f59cde5e1c9592a871999cff25c1f68
LilianaCaetano <lili.caetano1@gmail.com>
2019-08-17 14:29:35 +0100

    add random pictures to flat show and create a new flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -27,6 +27,7 @@ class FlatsController < ApplicationController
   def create
     @flat = Flat.new(flat_params)
     @flat.user = current_user
+    authorize @flat
     if @flat.save
       redirect_to flat_path(@flat)
     else
